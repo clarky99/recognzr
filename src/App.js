5b16f6b3411420136cb966b29afb9e8746fa24b5
Clarkonius Maximus <60491215+clarky99@users.noreply.github.com>
2021-11-19 11:57:11 -0500

    added state clear to app js

--- src/App.js
+++ src/App.js
@@ -27,6 +27,21 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''
+  }
+}
+
 class App extends Component {
 
   constructor() {
@@ -104,6 +119,7 @@ class App extends Component {
           .then(count => {
             this.setState(Object.assign(this.state.user, {entries: count}))
           })
+          .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))})
     .catch(err => console.log(err));
@@ -111,7 +127,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout'){
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route ==='home' ) {
       this.setState({isSignedIn: true})
     }
