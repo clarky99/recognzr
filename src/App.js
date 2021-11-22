90ceb8f74bc92f41b42c9bd11736809d6c5fd481
Clarkonius Maximus <60491215+clarky99@users.noreply.github.com>
2021-11-22 14:39:23 -0500

    preparing for heroku deploy

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
 
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://rocky-eyrie-36589.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -107,7 +107,7 @@ class App extends Component {
     .then(response => 
       {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://rocky-eyrie-36589.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
